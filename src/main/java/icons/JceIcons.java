78c2bee69c4352563dd194c6463e36b25c89ce13
harveyxu <harveyxu@tencent.com>
2020-05-07 14:36:02 +0800

    Add copyright

--- src/main/java/icons/JceIcons.java
+++ src/main/java/icons/JceIcons.java
@@ -1,17 +1,16 @@
-package icons;
+/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class JceIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/file.png");
-    public static final Icon MODULE = IconLoader.getIcon("/icons/module.png");
-    public static final Icon STRUCT = IconLoader.getIcon("/icons/struct.png");
-    public static final Icon ENUM = IconLoader.getIcon("/icons/enum.png");
-    public static final Icon INTERFACE = IconLoader.getIcon("/icons/interface.png");
-    public static final Icon CONST = IconLoader.getIcon("/icons/const.png");
-    public static final Icon KEY = IconLoader.getIcon("/icons/key.png");
-    public static final Icon FIELD = IconLoader.getIcon("/icons/field.png");
-    public static final Icon FUNCTION = IconLoader.getIcon("/icons/field.png");
-}
